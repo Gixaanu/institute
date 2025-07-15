fa67f5ef269c2d519d19aae0b262f7f766b2c846
Jershon <jershoncacho8@gmail.com>
2025-07-15 19:57:10 +0800

    trial run

---
+++ week3/jershontest.py
@@ -0,0 +1,8 @@
+name = 'friend'
+print(f'Hello {name}!')
+input_name = input('What is your name?\n')
+
+def given_name(name):
+	return name
+
+input_task = input(f'Hello {given_name(input_name)}! what can I do for you?\n')
