11998f591c20a24641e2b9bc284e02bd825a9ab3
Dallin <nillad12@gmail.com>
2025-07-15 19:36:21 +0800

    python introduction

---
+++ week3/main.py
@@ -0,0 +1,50 @@
+# THIS IS A COMMENT
+
+# assignments
+# if statements
+# while loops
+# functions
+
+# ASSIGNMENTS
+# You have several different data types, e.g. int, float, string, boolean
+
+# This is an int
+num = 25
+
+# if num < 10:
+#   print('The number is less than 10')
+# elif num < 20:
+#   print('The number is less than 20')
+# else:
+#   print('The number is massive')
+
+# This is a float
+decimal = 5.0
+
+# This is a string
+name_1 = 'Junmar'
+name_2 = "Jershon"
+name_3 = "Lalaine's cat"
+
+# name = input('What is your name?\n')
+# print(name)
+
+#print(num)
+# count = 10
+# while count > 0:
+#   print('Hello everyone ', count)
+#   count = count - 1
+
+
+# FUNCTIONS
+def my_function(name = 'George'):
+  print(f'Hello {name}')
+
+my_function('Lalaine')
+my_function('Jershon')
+my_function('Junmar')
+my_function()
+
+print('Hello Lalaine')
+print('Hello Jershon')
+print('Hello Junmar')
